133613f066b3e60bbe738db70f17c156cfef8c72
jason13official <jason13official@gmail.com>
2024-09-12 21:06:58 -0400

    fix: remove legacy neoforge module

--- neoforge/src/main/java/com/cursee/new_slab_variants/core/common/registry/ModBlockEntityTypesNeoForge.java
+++
@@ -1,6 +0,0 @@
-package com.cursee.new_slab_variants.core.common.registry;
-
-public class ModBlockEntityTypesNeoForge {
-
-    public static void register() {}
-}
--- neoforge/src/main/java/com/cursee/new_slab_variants/core/common/registry/ModItemsNeoForge.java
+++
@@ -1,6 +0,0 @@
-package com.cursee.new_slab_variants.core.common.registry;
-
-public class ModItemsNeoForge {
-
-    public static void register() {}
-}
