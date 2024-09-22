0cb7acd23a838b3944a33b020137b3bcc667972a
jason13official <jason13official@gmail.com>
2024-09-22 12:46:55 -0400

    fix: refactor

---
+++ common/src/main/java/com/cursee/new_slab_variants/NewSlabVariants.java
@@ -0,0 +1,8 @@
+package com.cursee.new_slab_variants;
+
+public class NewSlabVariants {
+
+    public static boolean debugCommon = false;
+
+    public static void init() {}
+}
---
+++ fabric/src/main/java/com/cursee/new_slab_variants/NewSlabVariantsFabric.java
@@ -0,0 +1,17 @@
+package com.cursee.new_slab_variants;
+
+import com.cursee.monolib.core.MonoLibConfiguration;
+import com.cursee.monolib.core.sailing.Sailing;
+import net.fabricmc.api.ModInitializer;
+
+public class NewSlabVariantsFabric implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        
+        NewSlabVariants.init();
+        Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
+
+        NewSlabVariants.debugCommon = MonoLibConfiguration.debugging;
+    }
+}
